745327a4a539a9e9a4d96e9b55f8f9fd6171e3aa
Nicolas <nicolas.trochut@orange.fr>
2020-06-29 11:56:17 +0200

    Création d'une classe Project

---
+++ src/main/java/com/codurance/training/tasks/Project.java
@@ -0,0 +1,24 @@
+package com.codurance.training.tasks;
+
+import java.util.List;
+
+public class Project {
+    String name;
+    List<Task> tasks;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
