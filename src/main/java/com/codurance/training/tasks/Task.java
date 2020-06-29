2ed9829402e31c11f303c0803723c32c2721984d
Nicolas <nicolas.trochut@orange.fr>
2020-06-29 11:50:34 +0200

    Ajout d'une date de création de tâche

--- src/main/java/com/codurance/training/tasks/Task.java
+++ src/main/java/com/codurance/training/tasks/Task.java
@@ -1,14 +1,18 @@
 package com.codurance.training.tasks;
 
+import java.util.Date;
+
 public final class Task {
     private final long id;
     private final String description;
     private boolean done;
+    private final Date createdAt;
 
     public Task(long id, String description, boolean done) {
         this.id = id;
         this.description = description;
         this.done = done;
+        this.createdAt = new Date();
     }
 
     public long getId() {
@@ -25,5 +29,9 @@ public final class Task {
 
     public void setDone(boolean done) {
         this.done = done;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
     }
 }
